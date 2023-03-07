455c6007d31c3b12ed1b88422bceb1cc5d549a42
Yuan Tian <yuantiananan@163.com>
2023-03-07 10:51:47 +0800

    first commit

---
+++ app/src/main/java/com/anso/learn/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.anso.learn
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
