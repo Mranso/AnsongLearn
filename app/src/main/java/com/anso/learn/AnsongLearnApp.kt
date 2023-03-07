7ea153fafecffbf2dbea37b4179758feeb5cab7c
Yuan Tian <yuantiananan@163.com>
2023-03-07 14:03:18 +0800

    增加base video模块

---
+++ app/src/main/java/com/anso/learn/AnsongLearnApp.kt
@@ -0,0 +1,20 @@
+package com.anso.learn
+
+import android.app.Application
+import android.content.Context
+
+/**
+ * author：Yuantian
+ * time：2023/3/7 13:40
+ */
+class AnsongLearnApp : Application() {
+
+    override fun attachBaseContext(base: Context?) {
+        super.attachBaseContext(base)
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+}
